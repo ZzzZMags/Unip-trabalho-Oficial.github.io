5b27f0bc86fd596cda40eb14aaceb36ebbe4ba5c
ZzzZ <147109054+ZzzZMags@users.noreply.github.com>
2024-05-15 11:02:43 -0300

    adicionando atualizacoes

--- javazao.js
+++ javazao.js
@@ -1,7 +1,18 @@
 const inputCheck = document.querySelector('#modo-noturno')
 const elemento = document.querySelector('body')
+const switchMode = document.querySelector('.switchMode')
+const tema  = document.querySelector('#tema')
+
+switchMode.onchange = function(){
+    if(tema.textContent === 'Tema Claro'){
+        tema.innerText = 'Tema Escuro'
+    }else{
+        tema.innerText = 'Tema Claro'
+    }
+}
 
 inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'light' : 'dark'
     elemento.setAttribute('data-bs-theme', modo)
 })
+
